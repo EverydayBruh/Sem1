e771c652ebc1e1c629f98a854b28855d24eb16e1
EverydayBruh <roliki2222222@gmail.com>
2021-11-24 12:45:59 +0300

    First commit

--- activities_site/finalsite/activities_board/urls.py
+++ activities_site/finalsite/activities_board/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import api_category, api_event, api_user
+from .views import api_category, api_event, api_user, api_event_detail
 
 urlpatterns = [
 	path('api/category', api_category),
 	path('api/events', api_event),
+	path('api/events/<int:pk>', api_event_detail),
 	path('api/users', api_user),
 ]
--- activities_site/finalsite/activities_board/views.py
+++ activities_site/finalsite/activities_board/views.py
@@ -1,21 +1,53 @@
 from django.http import JsonResponse
 from .models import Category, Event, User
 from .serializers import CategorySerializer, EventSerializer, UserSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view(['GET'])
 def api_category(request):
     if request.method == 'GET':
         category = Category.objects.all()
         serializer = CategorySerializer(category, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
+
+@api_view(['GET', 'POST'])
 def api_event(request):
     if request.method == 'GET':
         event = Event.objects.all()
         serializer = EventSerializer(event, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = EventSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+@api_view(['GET', 'PATCH', 'DELETE'])
+def api_event_detail(request, pk):
+    event = Event.objects.get(pk=pk)
+    if request.method == 'GET':
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+    elif request.method == 'PATCH':
+        serializer = EventSerializer(event, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        event.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET'])
 def api_user(request):
     if request.method == 'GET':
         user = User.objects.all()
         serializer = UserSerializer(user, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
